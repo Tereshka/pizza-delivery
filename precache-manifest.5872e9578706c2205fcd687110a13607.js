b6e5d67fbcf2842cab0991cc6a930115d23c5171
Tereshka <tereshka07@gmail.com>
2020-10-21 21:59:50 +0900

    Updates

---
+++ precache-manifest.5872e9578706c2205fcd687110a13607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45650d992ff7abad1feccfdde2af5277",
+    "url": "/pizza-delivery/index.html"
+  },
+  {
+    "revision": "1639cb59900a95f7fcfc",
+    "url": "/pizza-delivery/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "4ba798ac47b7cbc68372",
+    "url": "/pizza-delivery/static/js/2.fc94b6d1.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/pizza-delivery/static/js/2.fc94b6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1639cb59900a95f7fcfc",
+    "url": "/pizza-delivery/static/js/main.4c447990.chunk.js"
+  },
+  {
+    "revision": "84059a0f6fbda1c7b592",
+    "url": "/pizza-delivery/static/js/runtime-main.6af57fe0.js"
+  }
+]);
